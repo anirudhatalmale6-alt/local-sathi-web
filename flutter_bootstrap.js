f08270546d407546be57d2fcd132cb0a721f4ecf
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-03-05 18:01:14 +0000

    Deploy v1.6.0: New logo, 32 categories, WhatsApp button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1183274350" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "486195254" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
