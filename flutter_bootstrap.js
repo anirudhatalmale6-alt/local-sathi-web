0e74c07f47bea4da46130e8a6773354ac2aa4d43
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-03-05 16:22:42 +0000

    Deploy v1.4.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3397913559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2470405741" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
